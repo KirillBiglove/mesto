9a125edb110acecf185e0e8a271fd6c79e322654
Kirill Biglove <kirill.biglove@yandex.ru>
2021-04-26 13:04:30 +0300

    feat:fullo of JS + media 910 - 575px

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,36 @@
-const openPopupButton = document.querySelector('.profile__edit-button');
-const popup = document.querySelector('.popup');
-const closePopupButton = document.querySelector('.popup__close-btn');
+let openPopupButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let closePopupButton = document.querySelector('.popup__close-btn');
+let formElement = document.querySelector('.popup__container');
+let saveButton = document.querySelector('.popup__save-btn');
 
 function togglePopup(event) {
-  popup.classList.toggle('popup__open');
+  popup.classList.toggle('popup__opened');
 }
 
-openPopupButton.addEventListener('click', togglePopup)
+function openSaveEdit() {
+  popup.classList.add('.popup__opened');
+  nameValue = nameElemementValue.textContent;
+  aboutValue = aboutElemementValue.textContent;
+}
+
+function formSubmitHandler(evt) {
+	evt.preventDefault();
+  let profileName = document.querySelector('.profile__title');
+  let profileJob = document.querySelector('.profile__subtitle');
+  let nameInput = document.querySelector('.popup__input_change_profile-title');
+  let jobInput = document.querySelector('.popup__input_change_profile-subtitle');
+  let nameValue = document.querySelector('.popup__input_change_profile-title').value;
+  let aboutValue= document.querySelector('.popup__input_change_profile-subtitle').value;
+  let nameElemementValue = document.querySelector('h1');
+  nameElemementValue.textContent = nameValue;
+  let aboutElemementValue = document.querySelector('h3');
+  aboutElemementValue.textContent = aboutValue;
+}
+
+openPopupButton.addEventListener('click', togglePopup);
 closePopupButton.addEventListener('click', togglePopup);
+formElement.addEventListener('click', formSubmitHandler);
+formElement.addEventListener('submit', formSubmitHandler);
+saveButton.addEventListener('click', togglePopup);
+
