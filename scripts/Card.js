614b4f9dce83b5eb936ffcb7397ac3d28dd5ff54
Kirill Biglove <kirill.biglove@yandex.ru>
2021-06-08 18:23:43 +0300

    hotfix: card.js

--- scripts/Card.js
+++ scripts/Card.js
@@ -4,22 +4,22 @@ export default class Card {
     this._link = link;
     this._templateSelector = templateSelector;
 
-    const cardTemplate = document.querySelector(this._templateSelector).content.querySelector('.element');
-    this._cardElement = cardTemplate.cloneNode(true);
+    this._getTemplate();
 
     this._cardImage = this._cardElement.querySelector('.element__main-image');
     this._cardTitle = this._cardElement.querySelector('.element__text');
     this._likeButton = this._cardElement.querySelector('.element__button-like');
     this._trashButton = this._cardElement.querySelector('.element__delete-btn');
 
-
     this._openFullPhoto = openFullPhoto;
     this._fullPhoto = document.querySelector('.popup__image-full');
     this._fullPhotoTitle = document.querySelector('.popup__image-text');
 
-    this._setEventListeners();
   }
-
+  _getTemplate() {
+    const cardTemplate = document.querySelector(this._templateSelector).content.querySelector('.element');
+    this._cardElement = cardTemplate.cloneNode(true);
+  }
   _setEventListeners() {
     this._likeButton.addEventListener('click', () => this._handleSetLike());
     this._trashButton.addEventListener('click', () => this._handleDelCard());
@@ -47,6 +47,8 @@ export default class Card {
     this._cardImage.src = this._link;
     this._cardImage.alt = this._name;
     this._cardTitle.textContent = this._name;
+
+    this._setEventListeners();
 
     return this._cardElement;
   }
