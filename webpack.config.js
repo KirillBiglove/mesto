37bd9e3e7d077ee185538b2219d32a0b4a1c405f
Kirill Biglov <kirill.biglove@yandex.ru>
2021-06-21 17:20:03 +0300

    create webpck

--- webpack.config.js
+++ webpack.config.js
@@ -4,42 +4,42 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: { main: './src/index.js' },
+    entry: {
+        main: './src/index.js'
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js',
-        publicPath: ''
+        publicPath: '',
     },
-    mode: 'development', // добавление режима разработчика
+    mode: 'development',
     devServer: {
-        contentBase: path.resolve(__dirname, './dist'), // путь куда "смотрит" режим разработчика
-        compress: true, // это ускорит загрузку в режиме разработки
-        port: 9000, // порт, что бы открывать сайт по адресу
-        open: true // сайт будет открываться сам при запуске npm run dev
+        contentBase: path.resolve(__dirname, './dist'),
+        open: true,
+        compress: true,
+        port: 8080
     },
     module: {
-        rules: [ //массив правил
-            //добавим в него обхект правил для бабеля
-            { test: /\.js$/,
-                //при обработке использовать бабель
-                use: 'babel-loader',
-                //исключает папку node_modules
-                exclude: '/node_modules/' },
-
-                {
-                    test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-                    type: 'asset/resourse'
-                },
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, {
-                    loader: 'css-loader',
-                    options: { importLoaders: 1 }
-                },
-                'postcss-loader'],
-                //регулярное выражение, которое ищет все js файлы
-                
+        rules: [{
+            test: /\.js$/,
+            use: 'babel-loader',
+            exclude: '/node_modules/'
+        },
+        {
+            test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+            type: 'asset/resource',
+        },
+        {
+            test: /\.css$/,
+            use: [MiniCssExtractPlugin.loader, {
+                loader: 'css-loader',
+                options: {
+                    importLoaders: 1
+                }
             },
+                'postcss-loader'
+            ]
+        },
         ]
     },
     plugins: [
@@ -47,6 +47,7 @@ module.exports = {
             template: './src/index.html'
         }),
         new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin()
+        new MiniCssExtractPlugin(),
+
     ]
 }
